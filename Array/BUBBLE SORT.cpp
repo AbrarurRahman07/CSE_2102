e367509761e5a09197104e8b65290e3d0f1f8d10
Abrar Tahsin <Abrarur.cse@gmail.com>
2025-04-26 21:28:45 +0600

    update sort

--- Array/BUBBLE SORT.cpp
+++ Array/BUBBLE SORT.cpp
@@ -1,34 +1,36 @@
 #include<iostream>
 using namespace std;
 
-void sort_array(int a[],int x)
+void sort_array(int arr[],int n)
 {
-  for(int i=0;i<x;i++)
-  {
-    for(int j=i+1;j<x;j++)
+    for (int i = 0; i < n-1; i++)
     {
-      if(a[j]<a[i])
-        swap(a[j],a[i]);
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap(arr[j], arr[j+1]);
+            }
+        }
     }
-  }
 }
 int main()
 {
-  int arr[]={12,34,21,34,17,1,3,2,57,89,43,21,32,14,16,9,7,5};
-  int n=sizeof(arr)/sizeof(int);
-  cout<<"The given array is: "<<endl;
-  for(int i=0;i<n;i++)
-  {
-    cout<<arr[i]<<" ";
-  }
-  cout<<endl;
+    int arr[]= {12,34,21,34,17,1,3,2,57,89,43,21,32,14,16,9,7,5};
+    int n=sizeof(arr)/sizeof(int);
+    cout<<"The given array is: "<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 
-  sort_array(arr,n);
+    sort_array(arr,n);
 
-  cout<<"After sorting: "<<endl;
-  for(int i=0;i<n;i++)
-  {
-    cout<<arr[i]<<" ";
-  }
-  cout<<endl;
+    cout<<"After sorting: "<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 }
